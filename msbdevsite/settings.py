aa0666ac134767dea7f6e0283f641b989c9e0666
Matt Burlage <mattburlage@gmail.com>
2020-03-17 08:37:02 -0400

    db settings

--- msbdevsite/settings.py
+++ msbdevsite/settings.py
@@ -75,16 +75,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'msbdevsite.wsgi.application'
 
-
+#
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
