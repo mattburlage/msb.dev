c08825d34c28bb931bd3083ee2413db16de4f13f
matt burlage <mattburlage@gmail.com>
2020-05-16 22:52:43 -0400

    stuff

--- msbdevsite/devsettings.py
+++ msbdevsite/devsettings.py
@@ -12,3 +12,10 @@ SECRET_KEY = "asbfibasifbaskutdkdbvksakjb"
 
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
